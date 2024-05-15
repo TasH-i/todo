b27f63a5860edd0ffe953e503cc4cbe6df6c1c07
TasH-i <hansaneetashini@gmail.com>
2024-05-15 13:19:51 +0530

    create and read

--- app/src/main/java/com/mad/todo/MainActivity.kt
+++ app/src/main/java/com/mad/todo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.mad.todo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.mad.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +20,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
         binding.addButton.setOnClickListener {
             val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
-
-
     }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
+
 }
